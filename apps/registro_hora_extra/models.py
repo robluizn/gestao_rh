67897e13ddea1d501e1d5a4cb700f4f88eb11e81
Roberto Nunes <robluizn@yahoo.com.br>
2022-01-28 13:25:40 -0300

    CRUD de Horas Extras

--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -8,11 +8,11 @@ from apps.funcionarios.models import Funcionario
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
     funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
-    # horas = models.DecimalField(max_digits=5, decimal_places=2)
-    # utilizada = models.BooleanField(default=False)
-    #
-    # def get_absolute_url(self):
-    #     return reverse('update_funcionario', args=[self.funcionario.id])
+    horas = models.DecimalField(max_digits=5, decimal_places=2)
+    utilizada = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse('update_hora_extra', args=[self.id])
 
     def __str__(self):
         return self.motivo
